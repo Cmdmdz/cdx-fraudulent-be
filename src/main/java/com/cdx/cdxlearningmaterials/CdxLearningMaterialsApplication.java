afae5e2d237958eb3218addc1e8d1a153ce569c3
San5box <natkamon.meej@outlook.co.th>
2023-02-19 09:41:33 +0700

    first commit

---
+++ src/main/java/com/cdx/cdxlearningmaterials/CdxLearningMaterialsApplication.java
@@ -0,0 +1,13 @@
+package com.cdx.cdxlearningmaterials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdxLearningMaterialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdxLearningMaterialsApplication.class, args);
+    }
+
+}
